28a16a2fd62f274480f0e20c68f1aad223b1e5d7
fr33d00m <vice.simunov@gmail.com>
2020-02-28 11:27:55 +0100

    try

---
+++ src/main/java/com/dreamfactory/Main.java
@@ -0,0 +1,12 @@
+package com.dreamfactory;
+
+import com.licensespring.License;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("bla");
+        License myLicense = License.builder().build();
+
+    }
+}
