c1f1c58fa5be6414b180be83d8004d6225af0f56
fr33d00m <vice.simunov@gmail.com>
2020-02-28 23:33:37 +0100

    initial commit

--- src/main/java/com/dreamfactory/Main.java
+++ src/main/java/com/dreamfactory/Main.java
@@ -1,12 +1,52 @@
 package com.dreamfactory;
 
 import com.licensespring.License;
+import com.licensespring.LicenseManager;
+import com.licensespring.LicenseSpringConfiguration;
+import com.licensespring.dto.UnactivatedTrialLicense;
+import com.licensespring.model.ActivationLicense;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("bla");
-        License myLicense = License.builder().build();
 
+        LicenseSpringConfiguration configuration = LicenseSpringConfiguration.builder()
+                .apiKey("api-key")
+                .productCode("TT")
+                .sharedKey("shared-secret_key")
+                .appName("Test app")
+                .appVersion("3")
+                .build();
+
+        LicenseManager manager = LicenseManager.getInstance();
+
+        manager.initialize(configuration);
+
+        trial();
+
+        activate();
+
+        activateUser();
+
+    }
+
+    private static void activateUser() {
+        LicenseManager instance = LicenseManager.getInstance();
+        License license = instance.activateLicense(ActivationLicense.fromUsername("test", "pswd"));
+        instance.deactivateLicense(license.getIdentity());
+    }
+
+    private static void activate() {
+        LicenseManager instance = LicenseManager.getInstance();
+        License license = instance.activateLicense(ActivationLicense.fromKey("key"));
+        instance.deactivateLicense(license.getIdentity());
+    }
+
+    private static void trial(){
+        LicenseManager instance = LicenseManager.getInstance();
+        UnactivatedTrialLicense trialLicense = instance.getTrialLicense("test@test.com");
+        License license = instance.activateLicense(trialLicense.createIdentity());
+
+        assert instance.getCurrent().equals(license);
     }
 }
