aa1f09cc8d451c8208ccfa2f31c1b96fd0867213
Petra <petra.fribert@fer.hr>
2020-03-09 18:45:00 +0100

    lombok deÅpendency

--- src/main/java/com/dreamfactory/Main.java
+++ src/main/java/com/dreamfactory/Main.java
@@ -10,43 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LicenseSpringConfiguration configuration = LicenseSpringConfiguration.builder()
-                .apiKey("api-key")
-                .productCode("TT")
-                .sharedKey("shared-secret_key")
-                .appName("Test app")
-                .appVersion("3")
-                .build();
-
-        LicenseManager manager = LicenseManager.getInstance();
-
-        manager.initialize(configuration);
-
-        trial();
-
-        activate();
-
-        activateUser();
-
     }
 
-    private static void activateUser() {
-        LicenseManager instance = LicenseManager.getInstance();
-        License license = instance.activateLicense(ActivationLicense.fromUsername("test", "pswd"));
-        instance.deactivateLicense(license.getIdentity());
-    }
 
-    private static void activate() {
-        LicenseManager instance = LicenseManager.getInstance();
-        License license = instance.activateLicense(ActivationLicense.fromKey("key"));
-        instance.deactivateLicense(license.getIdentity());
-    }
-
-    private static void trial(){
-        LicenseManager instance = LicenseManager.getInstance();
-        UnactivatedTrialLicense trialLicense = instance.getTrialLicense("test@test.com");
-        License license = instance.activateLicense(trialLicense.createIdentity());
-
-        assert instance.getCurrent().equals(license);
-    }
 }
