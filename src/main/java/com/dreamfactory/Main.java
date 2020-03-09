6f2d999444efec843c45a104b6714953abcc8c30
Petra <petra.fribert@fer.hr>
2020-03-09 22:41:49 +0100

    reading configuration with yaml

--- src/main/java/com/dreamfactory/Main.java
+++ src/main/java/com/dreamfactory/Main.java
@@ -1,16 +1,41 @@
 package com.dreamfactory;
 
-import com.licensespring.License;
-import com.licensespring.LicenseManager;
+import com.google.common.io.ByteStreams;
 import com.licensespring.LicenseSpringConfiguration;
-import com.licensespring.dto.UnactivatedTrialLicense;
-import com.licensespring.model.ActivationLicense;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        Yaml yaml = new Yaml();
+        InputStream inputStream = Files.newInputStream(Paths.get("config.yaml"));
+        Map<String, String> obj = yaml.load(inputStream);
+        LicenseSpringConfiguration licenseSpringConfiguration = LicenseSpringConfiguration.builder()
+                .apiKey(obj.get("apiKey"))
+                .productCode(obj.get("productCode"))
+                .sharedKey(obj.get("sharedKey"))
+                .appName(obj.get("appName"))
+                .appVersion(obj.get("appVersion"))
+                //redundant
+                .serviceURL(obj.get("serviceURL"))
+                .build();
+
+        System.out.println(licenseSpringConfiguration);
 
     }
+
+
+
+
+
+
 
 
 }
