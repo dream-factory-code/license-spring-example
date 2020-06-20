db5728230df46f9fddae60c3d5c81088a8a0a167
Petra Fribert <petrafribert@gmail.com>
2020-06-20 15:38:06 +0200

    Refactor

--- src/main/java/com/dreamfactory/Configuration.java
+++ src/main/java/com/dreamfactory/Configuration.java
@@ -1,29 +1,24 @@
 package com.dreamfactory;
 
 import com.licensespring.LicenseSpringConfiguration;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 public class Configuration {
-    private String apiKey;
-    private String productCode;
-    private String sharedKey;
+    String apiKey;
+    String productCode;
+    String sharedKey;
+    String appName;
+    String appVer;
+    String serviceURL;
 
-    private String appName;
-    private String appVersion;
-
-    private String serviceURL = "https://api.licensespring.com/";
-
-    LicenseSpringConfiguration toLicenseSpringConfiguration(){
+    LicenseSpringConfiguration toLicenseSpringConfiguration() {
         return LicenseSpringConfiguration.builder()
                 .apiKey(apiKey)
                 .productCode(productCode)
                 .sharedKey(sharedKey)
                 .appName(appName)
-                .appVersion(appVersion)
+                .appVersion(appVer)
                 .serviceURL(serviceURL)
                 .build();
     }
--- src/main/java/com/dreamfactory/Main.java
+++ src/main/java/com/dreamfactory/Main.java
@@ -2,33 +2,36 @@ package com.dreamfactory;
 
 import com.licensespring.License;
 import com.licensespring.LicenseManager;
+import com.licensespring.hardware.HardwareInfo;
+import com.licensespring.hardware.MachineInfo;
 import com.licensespring.model.ActivationLicense;
 import com.licensespring.model.Product;
 import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
 
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        Yaml yaml = new Yaml();
-        Path path = Paths.get("config.yaml");
-        Configuration configuration = null;
+    public static void main(String[] args) throws IOException, GeneralSecurityException {
+        String path = "co.yaml";
+        Yaml yaml = new Yaml(new Constructor(Configuration.class));
+        InputStream inputStream = Main.class.getClassLoader()
+                .getResourceAsStream(path);
 
-        try (InputStream inputStream = Files.newInputStream(path)){
-            configuration = yaml.loadAs(inputStream, Configuration.class);
-        }
+        Configuration configuration = yaml.load(inputStream);
 
         LicenseManager manager = LicenseManager.getInstance();
         manager.initialize(configuration.toLicenseSpringConfiguration());
 
         Product product = manager.getProductDetails();
 
-        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
-            if (product.getAuthorizationMethod().equals("user")){
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            if (product.getAuthorizationMethod().equals("user")) {
                 System.out.println("\nPlease enter username:");
                 String username = reader.readLine();
 
@@ -42,9 +45,19 @@ public class Main {
             }
             System.out.println("\nLicense successfully activated.");
         }
+        License license = manager.getCurrent();
 
+        MachineInfo machineInfo = HardwareInfo.getMachineInfo();
+        String vmInfo = machineInfo.getVmInfo();
 
-}
+        System.out.println("Hardware ID >>> " + license.getHardwareId());
+        System.out.println("VM info >>> " + vmInfo);
+
+//        writing data to excel
+//        ExcelService service = new ExcelService(configuration,license);
+//        service.startDataTransfer();
+        System.exit(0);
+    }
 
     private static void activateUser(String username, String password) {
         LicenseManager instance = LicenseManager.getInstance();
